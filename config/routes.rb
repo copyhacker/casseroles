b0122e2c480427c627c4d59f9720fcbbc584d456
Jonathan Vaught <jvaught@elevatedrails.com>
2008-04-15 22:20:59 -0400

    Added contact form.

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ ActionController::Routing::Routes.draw do |map|
   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
   #     admin.resources :products
   #   end
+  
+  map.resources :contacts
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
