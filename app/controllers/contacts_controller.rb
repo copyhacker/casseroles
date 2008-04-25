a279f0658b53782789353bb6a8072a5e72df8255
Jonathan Vaught <jvaught@elevatedrails.com>
2008-04-25 10:37:29 -0400

    Mailer sends admin a BCC of the signup email to facilitate adding the contact to a mailing list.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,7 @@ class ContactsController < ActionController::Base
   def create
     @contact = Contact.create params[:contact]
     ContactsMailer.deliver_signup @contact
-    ContactsMailer.deliver_notify_admin @contact
+    # ContactsMailer.deliver_notify_admin @contact
     flash[:notice] = 'Thank you for signing up!'
     render :action => 'new'
   end
--- app/models/contacts_mailer.rb
+++ app/models/contacts_mailer.rb
@@ -1,19 +1,20 @@
 class ContactsMailer < ActionMailer::Base
   def signup(contact, sent_at = Time.now)
-    @subject    = 'Thank you for your interest in Caring Casseroles!'
+    @subject    = 'New Contact: Thank you for your interest in Caring Casseroles!'
     @body       = { :contact => contact }
-    @recipients = contact.email
-    @from       = 'no_reply@caringcasseroles.org'
+    @recipients = "#{contact.first_name} #{contact.last_name} <#{contact.email}>"
+    @bcc        = 'april@caringcasseroles.org'
+    @from       = 'contact@caringcasseroles.org'
     @sent_on    = sent_at
     @headers    = {}
   end
 
-  def notify_admin(contact, sent_at = Time.now)
-    @subject    = "#{contact.first_name} #{contact.last_name} has signed up for Caring Casseroles"
-    @body       = { :contact => contact }
-    @recipients = 'april@caringcasseroles.org'
-    @from       = 'no_reply@caringcasseroles.org'
-    @sent_on    = sent_at
-    @headers    = {}
-  end
+  # def notify_admin(contact, sent_at = Time.now)
+  #   @subject    = "#{contact.first_name} #{contact.last_name} has signed up for Caring Casseroles"
+  #   @body       = { :contact => contact }
+  #   @recipients = 'april@caringcasseroles.org'
+  #   @from       = 'contact@caringcasseroles.org'
+  #   @sent_on    = sent_at
+  #   @headers    = {}
+  # end
 end
