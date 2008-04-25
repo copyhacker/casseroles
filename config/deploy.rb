9c01ab0075720e8491818ed362ba4d735e3fdce6
Jonathan Vaught <jvaught@elevatedrails.com>
2008-04-25 12:18:52 -0400

    Changed Git repo to cab.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'mongrel_cluster/recipes'
 
 set :application, "casseroles"
-set :repository,  "git@github.com:gravelpup/casseroles.git"
+set :repository,  "git+ssh://casserolesdeploy@dev.elevatedrails.com/var/git/clients/casseroles"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
